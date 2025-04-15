95df9f0b26d58917dcced4390e4d17f798cceb7c
dev suwalka <devsuwalka1234@gmail.com>
2025-04-15 09:55:11 +0530

    ch

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,5 +1,5 @@
 import connectDB from '@/lib/db';
-import User from '/models/User';
+import User from './models/User';
 import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
--- pages/api/verify-otp.js
+++ pages/api/verify-otp.js
@@ -1,5 +1,5 @@
 import connectDB from '@/lib/db';
-import User from '/models/User';
+import User from './models/User';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
